4350ca04454ff7c330a3b3a1c3bb3f6227f3d9c9
mynewwfaccount2017 <mynewwfaccount2017@mail.ru>
2020-04-30 14:51:27 +0300

    New

---
+++ 2304_2_2/2304_2_2.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int F1(int x)
+{
+	if (x >= 0)
+	{
+		x = pow(double(x), 2);
+	}
+	else if (x < 0)
+	{
+		x = abs(x);
+	}
+
+	return x;
+}
+
+void F2(int &x)
+{
+	x = F1(x);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	for (int i = 1; i < 11; i++)
+	{
+		cout << "Параметр до передачи в функцию: " + to_string(i) + " ... ";
+		F2(i);
+		cout << "параметр после передачи в функцию: " + to_string(i) << endl;
+	}
+}
+
